682ce97aa547955eede6ecff7d30071d596fe1ce
Kyle Boss <kyle@kyleboss.com>
2018-01-16 18:28:06 -0800

    Removed unneeded RuboCop config

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # A Scan represents, in the physical world, the barcode scanner scanning a product. In the data world, it maps a
 # checkout with a specific product. There is a quantity column, if a product is scanned multiple times for a checkout,
 # we simply increment the quantity to save on space & calculation time.
